d9e13eae3a527427b9ee981951c0de0f50e666e2
Stephane_tcha <stephanetcha@gmail.com>
2023-01-31 21:21:47 +0100

    separation pdo

--- lib/recipe.php
+++ lib/recipe.php
@@ -5,3 +5,10 @@ $recipes = [
     ['title' => 'Gratin dauphinois', 'description' => 'Gratin quick example text to build on the card title and make up the bulk of the card\'s content.', 'image' => '2-gratin-dauphinois.jpg'],
     ['title' => 'Salade de chèvre', 'description' => 'Salade quick example text to build on the card title and make up the bulk of the card\'s content.', 'image' => '3-salade.jpg'],
 ];
+
+function getRecipeById(PDO $pdo,int $id) {
+    $query = $pdo->prepare("SELECT * FROM recipes WHERE id = :id");
+    $query->bindParam(':id', $id, PDO::PARAM_INT);
+    $query->execute();
+    $recipe = $query->fetch();
+}
--- recette.php
+++ recette.php
@@ -8,12 +8,7 @@ $pdo = new PDO('mysql:dbname=studi_live_cuisinea;host=localhost;charset=utf8mb4'
 
 $id = $_GET['id'];
 
-
-$query = $pdo->prepare("SELECT * FROM recipes WHERE id = :id");
-$query->bindParam(':id', $id, PDO::PARAM_INT);
-$query->execute();
-$recipe = $query->fetch();
-
+$recipe = getRecipeByID($pdo, $id);
 
 if($recipe){ 
     if($recipe['image'] === null){
