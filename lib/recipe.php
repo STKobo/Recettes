bb8e2be009e710d7642f371156f478d2f4317c6c
Stephane_tcha <stephanetcha@gmail.com>
2023-01-31 21:30:43 +0100

    minification du code

--- lib/recipe.php
+++ lib/recipe.php
@@ -12,3 +12,13 @@ function getRecipeById(PDO $pdo,int $id) {
     $query->execute();
     $recipe = $query->fetch();
 }
+
+
+function getRecipeImage(string $image){
+    if($image === null){
+        return _ASSETS_IMG_PATH_.'recipe_default.jpg';
+    } else {
+        return _RECIPES_IMG_PATH_.$image;
+        }
+    
+}
--- recette.php
+++ recette.php
@@ -4,18 +4,12 @@ require_once('lib/tools.php');
 require_once('lib/recipe.php');
 
 
-$pdo = new PDO('mysql:dbname=studi_live_cuisinea;host=localhost;charset=utf8mb4', 'root', '');
 
 $id = $_GET['id'];
 
 $recipe = getRecipeByID($pdo, $id);
 
 if($recipe){ 
-    if($recipe['image'] === null){
-        $imagePath = _ASSETS_IMG_PATH_.'recipe_default.jpg';
-    } else {
-        $imagePath = _RECIPES_IMG_PATH_.$recipe['image'];
-        }
     
     $ingredients = linesToArray($recipe['ingredients']);
     $instructions = linesToArray($recipe['instructions']);
@@ -24,7 +18,7 @@ if($recipe){
 
 <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
     <div class="col-10 col-sm-8 col-lg-6">
-        <img src="<?= $imagePath; ?>" class="d-block mx-lg-auto img-fluid" alt="<?=$recipe['title']; ?>" width="700" height="500" loading="lazy">
+        <img src="<?= getRecipeImage($recipe['image']); ?>" class="d-block mx-lg-auto img-fluid" alt="<?=$recipe['title']; ?>" width="700" height="500" loading="lazy">
     </div>
     <div class="col-lg-6">
         <h1 class="display-5 fw-bold lh-1 mb-3"><?=$recipe['title']; ?></h1>
