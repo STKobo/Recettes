922080cf85c32849923f5c2ee20beaee1f915d83
Stephane_tcha <stephanetcha@gmail.com>
2023-02-09 20:42:21 +0100

    securité bindParam pour injection sql

--- lib/recipe.php
+++ lib/recipe.php
@@ -19,7 +19,16 @@ function getRecipeImage(string|null $image){
 function getRecipes(PDO $pdo, int $limit = null){
     $sql = 'SELECT * FROM recipes ORDER BY id DESC';
 
+    if($limit) {
+        $sql .= ' LIMIT :limit';
+    }
+
     $query = $pdo->prepare($sql);
+
+    if($limit){
+        $query->bindParam(':limit', $limit, PDO::PARAM_INT);
+    }
+    
     $query->execute();
     $recipes = $query->fetchAll(); 
 }
