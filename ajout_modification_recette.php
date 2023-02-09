6752a026bfd73cc55c7b4fd9cd8fe1da4ed1ece5
Stephane_tcha <stephanetcha@gmail.com>
2023-02-09 21:16:36 +0100

    formulaire

--- ajout_modification_recette.php
+++ ajout_modification_recette.php
@@ -6,6 +6,35 @@ require_once('lib/recipe.php');
 ?>
 
 <form method="POST" enctype="multipart/form-data">
+    <div class="mb-3">
+        <label for="title" class="form-label">Titre</label>
+        <input type="text" name="title" id="title" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label for="description" class="form-label">Description</label>
+        <textarea name="description" id="description" cols="30" rows="5"></textarea>
+    </div>
+    <div class="mb-3">
+        <label for="ingredients" class="form-label">Ingredients</label>
+        <textarea name="ingredients" id="ingredients" cols="30" rows="5"></textarea>
+    </div>
+    <div class="mb-3">
+        <label for="instructions" class="form-label">Instructions</label>
+        <textarea name="instructions" id="instructions" cols="30" rows="5"></textarea>
+    </div>
+    <div class="mb-3">
+        <label for="category" class="form-label">Catégorie</label>
+        <select name="category" id="category" class="form-select">
+            <option value="1">Entrée</option>
+            <option value="2">Plat</option>
+            <option value="3">Dessert</option>
+        </select>
+    </div>
+    <div>
+        <label for="file" class="form-label">Image</label>
+        <input type="file" name="file" id="file">
+    </div>
+    <input type="submit" value="Enregistrer" name="saveRecipe" class="btn btn-primary">
 
 </form>
 
